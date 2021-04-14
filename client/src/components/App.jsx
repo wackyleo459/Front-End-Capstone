7d253c39f8a2ab245bb4b9260f080c3da59aa989
Darien <darienchang10@gmail.com>
2021-04-14 16:28:07 -0700

    Folders created

---
+++ client/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      something: []
+    }
+  }
+
+
+  render() {
+    return (
+    <div>
+      Does this work?
+    </div>
+    )
+  }
+
+
+}
+
+export default App;
