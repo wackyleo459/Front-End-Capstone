820aca69696f2ac338cf9b6ec5f8ecb06419c695
Nader <naderdamouni2@gmail.com>
2021-04-26 10:03:47 -0700

    Completed star rating component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,10 +7,10 @@ class App extends React.Component {
     super(props)
 
     this.state = {
-      something: []
+      selectedReview: {}
     }
-  }
 
+  }
 
   render() {
     return (
--- client/src/components/ratingsAndReviews/ReviewEntry.jsx
+++ client/src/components/ratingsAndReviews/ReviewEntry.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import StarRatingComponent from 'react-star-rating-component';
-import { FaStar } from 'react-icons/fa'
+import { ImStarFull, ImStarHalf, ImStarEmpty } from 'react-icons/im'
+import Modal from 'react-awesome-modal';
 
-const ReviewEntry = ({ review, helpfulChange }) => (
+
+const ReviewEntry = ({ reviews, review, helpfulChange, openImageModal, closeImageModal, visible }) => {
+  let ratingResult = 0;
+  ratingResult += review.rating / reviews.length;
+  let ratingRounded = Math.round(ratingResult * 10) / 10;
+  return (
+
   <div>
     <div className="starUserDateNd">
       <span>
         <StarRatingComponent
             name="rate2"
             editing={false}
-            renderStarIcon={() => <span><FaStar/></span>}
             starCount={5}
             value={review.rating}
+            renderStarIcon={review.rating <= 1 ? () => <span><ImStarFull/></span> : () => <span><ImStarEmpty/></span>}
+            renderStarIcon={review.rating <= 2 ? () => <span><ImStarFull/></span> : () => <span><ImStarEmpty/></span>}
+            renderStarIcon={review.rating <= 3 ? () => <span><ImStarFull/></span> : () => <span><ImStarEmpty/></span>}
+            renderStarIcon={review.rating <= 4 ? () => <span><ImStarFull/></span> : () => <span><ImStarEmpty/></span>}
+            renderStarIcon={review.rating <= 5 ? () => <span><ImStarFull/></span> : () => <span><ImStarEmpty/></span>}
+            renderStarIconHalf={() => <span><ImStarHalf/></span>}
         />
       </span>
-      <span>{new Date(review.date).toString().slice(0, 16)}</span>
+      <span>{review.reviewer_name}, {new Date(review.date).toString().slice(0, 16)}</span>
     </div>
     <div>
       {review.summary.length > 50 ?
@@ -23,15 +35,22 @@ const ReviewEntry = ({ review, helpfulChange }) => (
       <h2 className="reviewTitleNd">{review.summary}</h2>
       }
       <p className="reviewFeedBodyNd">{review.body}</p>
+
+      <p className="reviewRecommendNd">{review.recommend}</p>
+      <input className="openModalPhotoNd" type="button" value="Profile Picture" onClick={() => openImageModal()}/>
+      <Modal visible={visible} width="800" height="500" effect="fadeInUp" onClickAway={() => closeImageModal()}>
+        {/* {!review.photos ? null :
+        <img src={review.photos}/>} */}
+      </Modal>
       {!review.response ? null :
       <div className="reviewResponseNd">
-        <b>Response:</b>
-        <p>{review.response}</p>
+        <b>Response:{review.response}</b>
         </div>}
-
       <a onClick={(event) => helpfulChange(event)}>Helpful? <u>Yes</u>({review.helpfulness}) | <u>Report</u></a>
     </div>
     <hr/>
   </div>
-)
+  )
+}
+
 export default ReviewEntry;
