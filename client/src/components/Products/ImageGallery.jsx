b11fa13e24fa35e69ac13dd4bad7bf97cf0d2736
Sue Ann Kim <kimsueann@gmail.com>
2021-04-27 00:52:25 -0700

    Thumbnail Scrolls to always show selected thumbnail

--- client/src/components/Products/ImageGallery.jsx
+++ client/src/components/Products/ImageGallery.jsx
@@ -3,7 +3,7 @@ import React, { useState, useRef } from 'react';
 const ImageGallery = function (props) {
   const [currentIndex, setIndex] = useState(0);
 
-  const thumbnailClick = (e, link, index) => {
+  const thumbnailClick = (e, index) => {
     e.preventDefault();
     setIndex(index);
   }
@@ -11,10 +11,16 @@ const ImageGallery = function (props) {
   const arrowsClick = (e, direction) => {
     e.preventDefault();
     if (direction === 'left' && currentIndex > 0) {
+      if (currentIndex === 7) {
+        slideThumbnails('left');
+      }
       setIndex((prevInd) => {
         return prevInd - 1;
       })
     } else if (direction === 'right' && currentIndex < props.detail.length - 1) {
+      if (currentIndex === 6) {
+        slideThumbnails('right');
+      }
       setIndex((prevInd) => {
         return prevInd + 1;
       })
@@ -41,7 +47,7 @@ const ImageGallery = function (props) {
 
   //onHover w/ magnifying '+' symbol
   //zoom 2.5*
-  //style={{backgroundImage:`url(${props.detail[currentIndex].url})`}}
+
   return (
     <div className="imageGallery">
       <div className="overlay">
@@ -50,7 +56,7 @@ const ImageGallery = function (props) {
         <div className="thumbnailContainer" >
           {props.detail.map((item, ind) => (
             <img className="thumbnail" key={ind} src={item.thumbnail_url}
-              onClick={(e) => thumbnailClick(e, item, ind)}
+              onClick={(e) => thumbnailClick(e, ind)}
               style={thumbnailStyle(ind)} />
           ))}
         </div>
@@ -74,19 +80,4 @@ const ImageGallery = function (props) {
 }
 export default ImageGallery;
 
-{/* <Carousel activeIndex={index} onSelect={handleSelect}>
-        <Carousel.Item>
-          <img className="mainImg" src="https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/2af1560aab574695a3b4ac7200d483c9_9366/4D_Fusio_Shoes_Orange_FY5929_01_standard.jpg" alt="first slide" />
-          <Carousel.Caption>
-            <p>caption as paragraph tag.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="mainImg" src="https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/69d47e8502c2490d94e9ac5c0158e3ff_9366/Multix_Shoes_Red_H01896_41_detail.jpg" alt="second slide" />
-          <Carousel.Caption>
-            <p>caption as paragraph tag.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
 
-      </Carousel> */}
-      // <img className="mainImg" src="https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy/69d47e8502c2490d94e9ac5c0158e3ff_9366/Multix_Shoes_Red_H01896_41_detail.jpg" alt="second slide" />
