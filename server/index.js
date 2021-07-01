b200ed0a083daed11ac5783e0f93921c76add429
Sue Ann Kim <kimsueann@gmail.com>
2021-07-01 14:44:34 -0700

    process env for port

--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const path = require('path');
-const router = require('./router.js');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+const router = require("./router.js");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
-app.use('/', router);
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
+app.use(morgan("dev"));
+app.use("/", router);
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.listen(PORT, () => {
-  console.log(`Connected to port ${PORT}`)
-})
-
+  console.log(`Connected to port ${PORT}`);
+});
